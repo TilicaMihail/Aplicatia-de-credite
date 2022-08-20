2e27d355bf4cf4e51b86348894677e9cad46f5e4
mihail tilica <mihailtilica1@gmail.com>
2022-08-20 15:49:34 +0300

    made auth context

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,31 +1,53 @@
 import React from 'react'
+import { apiUrl } from '../apiUrl'
 
 const AuthContext = React.createContext({})
 
-const login = (authInfo, setError) => {
-    try {
-        const res = await axios.post('/login')
-    } catch (error) {
-        setError(error)
-    }
-}
-
-const register = (authInfo, setError) => {
-
-}
-
-const logout = () => {
-    try {
-        const res = await axios.post('/logout')
-    } catch (error) {
-        
-    }
-}
-
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState()
+
+    const login = async (authInfo, setError) => {
+        try {
+            const res = await axios.post(`${apiUrl}/auth/login`, authInfo)
+            setUser(res.data)
+        } catch (error) {
+            setError(error)
+        }
+    }
+    
+    const register = async (authInfo, setError) => {
+        try {
+            const res = await axios.post(`${apiUrl}/auth/register`, authInfo)
+            setUser(res.data)
+        } catch (error) {
+            setError(error)
+        }
+    }
+    
+    const logout = async () => {
+        try {
+            await axios.post(`${apiUrl}/auth/logout`)
+            setUser(undefined)
+        } catch (error) {
+            
+        }
+    } 
+
+    const getUser = async () => {
+        try {
+            const res = await axios.get(`${apiUrl}/users/current-user`)
+            setUser(res.data)
+        } catch (error) {
+            setUser(undefined)
+        }
+    }
+
+    useEffect(() => {
+        getUser();
+    }, [])
+
     return (
-        <AuthContext.Provider value = {{user, setUser}}>
+        <AuthContext.Provider value = {{ user, setUser, login, register, logout, getUser }}>
             { children }
         </AuthContext.Provider>
     )
