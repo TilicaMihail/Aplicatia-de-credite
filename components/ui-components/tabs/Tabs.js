445f957a68095b79447a66fd0f63d1b9c1dfa96a
mihail tilica <mihailtilica1@gmail.com>
2022-09-01 16:39:15 +0300

    a

--- components/ui-components/tabs/Tabs.js
+++ components/ui-components/tabs/Tabs.js
@@ -10,7 +10,7 @@ const Tabs = ({ tabs }) => {
                         return (
                             <div 
                                 key = {index} 
-                                className = {'p-2 text-lg cursor-pointer hover:bg-gray-200 rounded-t-lg border-b-4 ' + (selected === index ? ' font-bold border-blue-400' : 'border-gray-100 ')} 
+                                className = {'p-2 text-lg cursor-pointer hover:bg-gray-200 rounded-t-lg border-b-4 ' + (selected === index ? ' font-bold border-black' : 'border-gray-100 ')} 
                                 onClick = {() => { setSelected(index)}}
                             >
                                 {tab.label}
