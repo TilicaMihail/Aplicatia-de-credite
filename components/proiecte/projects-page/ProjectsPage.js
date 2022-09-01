a4af87813cdfcd4ec689f5443bf00dd7bac502f2
mihail tilica <mihailtilica1@gmail.com>
2022-09-01 16:02:09 +0300

    added tabs array

--- components/proiecte/projects-page/ProjectsPage.js
+++ components/proiecte/projects-page/ProjectsPage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../../contexts/AuthContext'
 import Header from '../../layout/Header'
 import Tabs from '../../ui-components/tabs/Tabs'
+import AdvancedSection from '../sections/AdvancedSection'
+import VluntieeringSection from '../sections/VluntieeringSection'
+import UnapprovedSection from '../sections/UnapprovedSection'
+import SignedUpProjects from '../../stats/SignedUpProjects'
+import CreatedProjects from '../../stats/CreatedProjects'
 
 const ProjectsPage = () => {
+    const { user } = useContext(AuthContext)
     const tabs = [
-        { label: 'Avansat', component: <></>}
+        { label: 'Avansat', component: <AdvancedSection /> },
+        { label: 'Optionale modulare', component: <VluntieeringSection /> },
+        { label: 'Neaprobate', component: <UnapprovedSection />},
+        { label: 'Proiectele mele', component: <SignedUpProjects />},
+        { label: 'Proiecte create', component: <CreatedProjects />}
     ]
 
     return (
