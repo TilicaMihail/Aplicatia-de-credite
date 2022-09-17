a9e9cd6a6a000e619fd84be5110ce3fe92058a1a
mihail tilica <mihailtilica1@gmail.com>
2022-09-17 20:52:57 +0300

    added elevi section

---
+++ components/elevi/Elev.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Elev = ({ student, index }) => {
+    return (
+        <div className = 'w-full p-3 bg-white shadow-lg rounded-xl flex justify-between card-hover '>
+            <div className = 'font-bold flex item-center justify-center'>
+                <div className = 'pr-3'>
+                    {
+                        index === 0 ? 
+                            <img src = {'https://i.pinimg.com/originals/86/d9/64/86d9642da31c8ebb28cb0ea5693b08eb.png'} alt = '' className = 'h-6' />
+                        : index === 1 ? 
+                            <div>
+                                <img src = {'https://bjstrophy.com/image/cache/catalog/FE%20Medals/FE-282-700x700.jpg'} alt = '' className = 'h-6' />
+                            </div>
+                        : index === 2 ?
+                            <div>
+                                <img src = {'https://www.pngall.com/wp-content/uploads/4/Third-Place-PNG-Picture.png'} alt = '' className = 'h-6' />
+                            </div>
+                        :
+                            index  + 1 + '. '
+                    }
+                </div>
+                <div>
+                    { student?.email}
+                </div>
+            </div>
+            <div className = 'flex items-center gap-4'>
+                { student?.totalCredite}
+                <ion-icon name="trophy-outline"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
+export default Elev
--- components/elevi/EleviPage.js
+++ components/elevi/EleviPage.js
@@ -1,24 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { UsersContext } from '../../contexts/UsersContext'
 import Header from '../layout/Header'
+import Elev from './Elev'
 
 const EleviPage = () => {
 
-    const { students } = useContext(UsersContext)
+    const { students, getStudents } = useContext(UsersContext)
+
+    useEffect(() => {
+        getStudents()
+    }, [])
 
     return (
         <div>
             <Header />
             <div className = 'p-8'>
-                <div className = 'font-bold text-xl sm:text-2xl'>
+                <div className = 'font-bold text-xl sm:text-2xl pb-4'>
                     Elevi 
                 </div>
-                <div>
+                <div className = 'flex flex-col gap-3'>
                     {
                         students?.map((student, index) => {
                             return (
                                 <div key = {index}>
-                                    aaa
+                                    <Elev student = {student} index = {index} />
                                 </div>
                             )
                         })
