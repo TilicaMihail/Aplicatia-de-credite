c27bb88b93886499f2a4ca54893cef723a5d6e8a
mihail tilica <mihailtilica1@gmail.com>
2022-10-08 18:30:32 +0300

    added imgs to img picker

--- components/ui-components/modals/ImagePicker.js
+++ components/ui-components/modals/ImagePicker.js
@@ -26,7 +26,8 @@ const ImagePicker = ({ setImgUrl, imgUrl, visible, setVisible }) => {
         'https://unblast.com/wp-content/uploads/2021/09/Data-Analytic-Process-Illustration.jpg',
         'https://unblast.com/wp-content/uploads/2022/08/Programmer-Illustration-0.jpg',
         'https://unblast.com/wp-content/uploads/2020/04/Female-Doctor-Vector-Illustration.jpg',
-
+        'https://miro.medium.com/max/750/1*sFaAHMeBX3GnhkL1CW7sTA.jpeg',
+        'https://media.istockphoto.com/vectors/young-adult-woman-reading-book-relaxing-sitting-in-chair-under-lamp-vector-id1159363620?k=20&m=1159363620&s=612x612&w=0&h=pY0TU2BqItqrRf-MJMkAxiy8uVCD1O76rjQJBVaa78o=',
     ])
 
     return (
