9e2dd2fe75c9bf20dc118bd096d3f715f4f83c1c
mihail tilica <mihailtilica1@gmail.com>
2022-10-08 13:55:43 +0300

    added first part of profile page

--- components/profile/ProfilePage.js
+++ components/profile/ProfilePage.js
@@ -28,34 +28,33 @@ const ProfilePage = () => {
 
     return (
         <div>
-            <div className = 'flex m-8 gap-8 flex-wrap'>
-                <div className = 'flex flex-col bg-white rounded-xl shadow-lg overflow-hidden md:w-1/2 w-full'>
-                    <div>
+            <div className = 'flex m-10 gap-8 flex-wrap'>
+                <div className = 'flex overflow-hidden w-full p-6 justify-between bg-white rounded-xl shadow-lg'>
+                    <div className = 'p-2'>
+                        <div className = 'font-bold text-xl md:text-2xl lg:text-4xl'> {userById?.firstName} {userById?.lastName} </div>
+                        <div className = 'text-lg'> Email: {userById?.email} </div>
+                        <div className = 'text-lg'> Rol: {userById?.role} </div>
+                        {
+                            userById?.role === 'elev' && 
+                            <>
+                                <div className = 'text-lg'>
+                                    Clasa: {userById?.clasa + userById?.profil}
+                                </div>
+                                <div className = 'text-lg'>
+                                    Credite: {userById?.totalCredite}
+                                </div>
+                            </>
+                        }
+                    </div>
+                    <div className = 'flex items-center justify-center flex-col gap-2'>
                         <img 
-                            className = 'h-20 w-20 rounded-full overflow-hidden border'
+                            className = 'h-28 w-28 rounded-full overflow-hidden border'
                             src = {profileData?.img || 'https://img.myloview.com/stickers/user-icon-vector-people-icon-profile-vector-icon-person-illustration-business-user-icon-users-group-symbol-male-user-symbol-400-223068872.jpg'} alt = '' 
                         />
                     </div>
-                    <div>
-                        <div className = 'grow-0 bg-red-500'> {userById?.firstName} {userById?.lastName} </div>
-                        <div className = 'grow-0'>  </div>
-                        <div className = 'grow-0'> {userById?.phoneNumber} </div>
-                        <div className = 'grow-0'> {userById?.role} </div>
-                    </div>
                 </div>
-                <div className = 'flex flex-col bg-white rounded-xl shadow-lg overflow-hidden grow'>
-                    <div>
-                        a
-                    </div>
-                    <div>
-                        
-                    </div>
-                    <div>
-                    
-                    </div>
-                    <div>
-                    
-                    </div>
+                <div className = 'asdf'>
+
                 </div>
             </div>
             <div>
