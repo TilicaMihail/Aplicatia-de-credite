32de6d6a2eb9c9aa6e8867ecafd8db77e7eae736
mihail tilica <mihailtilica1@gmail.com>
2022-10-09 15:59:01 +0300

    fixed responsivenes of image picker

--- components/ui-components/modals/ImagePicker.js
+++ components/ui-components/modals/ImagePicker.js
@@ -54,8 +54,8 @@ const ImagePicker = ({ setImgUrl, imgUrl, visible, setVisible }) => {
                                 }
                             </div>
                             <div className = 'flex items-center justify-center mt-2'>
-                                <input className = 'border outline-none grow p-3 m-1 rounded-xl' value = {customUrl} onChange = {e => {setCustomUrl(e.target.value); setError('')}} placeholder = 'Enter image address' />
-                                <div className = 'btn btn-info text-white' onClick = {() => {customUrl !== '' ? setImgUrl(customUrl) : setError('Custom url input is empty')}}>
+                                <input className = 'border outline-none grow p-3 m-1 rounded-xl w-full' value = {customUrl} onChange = {e => {setCustomUrl(e.target.value); setError('')}} placeholder = 'Enter image address' />
+                                <div className = 'btn btn-info text-white text-sm' onClick = {() => {customUrl !== '' ? setImgUrl(customUrl) : setError('Custom url input is empty')}}>
                                     Add custom image
                                 </div>
                             </div>
