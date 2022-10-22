71286bf19bfd890d31698c5754f29125ede909da
mihail tilica <mihailtilica1@gmail.com>
2022-10-22 12:10:58 +0300

    added 1 btn

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 .env variables are: NEXT_PUBLIC_DEFAULT_API_URL
+
+Pentru a face functionalitatea de fise cu credite care se pot downloada uite-te in ./components/elevi/EleviPage.js 
--- components/elevi/EleviPage.js
+++ components/elevi/EleviPage.js
@@ -1,12 +1,13 @@
 import { info } from 'daisyui/src/colors'
 import React, { useContext, useEffect } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 import { SearchContext } from '../../contexts/SearchContext'
 import { UsersContext } from '../../contexts/UsersContext'
 import Header from '../layout/Header'
 import Elev from './Elev'
 
 const EleviPage = () => {
-
+    const { user } = useContext(AuthContext)
     const { students } = useContext(UsersContext)
     const { searchFilter } = useContext(SearchContext)
 
@@ -14,8 +15,15 @@ const EleviPage = () => {
         <div>
             <Header />
             <div className = 'p-8'>
-                <div className = 'font-bold text-xl sm:text-2xl pb-4'>
-                    Elevi 
+                <div className = 'flex items-center justify-between pb-4'>
+                    <div className = 'font-bold text-xl sm:text-2xl '>
+                        Elevi 
+                    </div>
+                    { user?.role === 'admin' &&
+                    <div className = 'btn btn-info text-white'>
+                        Obtine fisele de credite
+                    </div>
+                    }
                 </div>
                 <div className = 'flex flex-col gap-3'>
                     {
